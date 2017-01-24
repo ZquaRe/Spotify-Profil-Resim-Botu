7de3455b59adfb6c7b7e25ca4163bcbfb726eb5b
Furkan Sezgin <samp_leon@hotmail.com>
2017-01-24 03:04:27 +0200

    Update Spotify-Resim-Botu.php

--- Spotify-Resim-Botu.php
+++ Spotify-Resim-Botu.php
@@ -1,6 +1,6 @@
 <?php
 
-
+$site=@file_get_contents("https://open.spotify.com/user/11100041149");
 @preg_match_all('@ <style>(.*?)  </style>@si',$site,$Resim);
 @$Resim = explode ("//",$Resim[1][0]);
 @$Resim = explode (") center",$Resim[1]);
